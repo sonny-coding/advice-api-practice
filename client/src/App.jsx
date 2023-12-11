ede64c4447255ed02c12b4b2e66f6d348d8d0960
Son Dao <udemydaohsn@gmail.com>
2023-12-11 10:46:10 -0700

    add client

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Advice from "./components/Advice";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline font-manrope">
+        Hello world!
+      </h1>
+      <Advice />
+    </>
+  );
+}
---
+++ client/src/components/Advice.jsx
@@ -0,0 +1,11 @@
+const Advice = () => {
+  return (
+    <div>
+      <p>advice #117</p>
+      <p>It is easy to sit up and take notice</p>
+      <button>Another one!</button>
+    </div>
+  );
+};
+
+export default Advice;
